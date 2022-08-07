c55f1af111372a514d8a5f0b4b64a51adb634d1e
DennisDv24 <jokarboxing@gmail.com>
2022-08-07 17:24:06 +0200

    implementing bridge

---
+++ contracts/NewCollectionSuggester.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: VPL
+pragma solidity ^0.8.0;
+
+import "@fxportal/contracts/tunnel/FxBaseRootTunnel.sol";
+
+contract NewCollectionSuggester is FxBaseRootTunnel {
+
+    constructor(
+		address _checkpointManager, address _fxRoot
+	) FxBaseRootTunnel(_checkpointManager, _fxRoot) { }
+
+    function _processMessageFromChild(bytes memory message) 
+		internal 
+		virtual
+		override
+	{ }
+
+	function suggestNewCollection(address newCollection) public {
+		// address verification logic
+		_sendMessageToChild(abi.encode(newCollection));
+	}
+ 
+}
